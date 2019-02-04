2ec3692a78b6579c70efff63e11c12947f055c5c
Winston Durand <me@winstondurand.com>
2019-02-03 21:06:29 -0800

    Fix namespacing

--- lib/backblaze/b2.rb
+++ lib/backblaze/b2.rb
@@ -8,71 +8,73 @@ require 'net/http'
 require 'tempfile'
 require 'digest/sha1'
 
-module Backblaze::B2
-  class << self
-    attr_reader :account_id, :token, :api_url, :download_url, :api_path, :min_part_size, :default_concurrency
+module Backblaze
+  module B2
+    class << self
+      attr_reader :account_id, :token, :api_url, :download_url, :api_path, :min_part_size, :default_concurrency
 
-    ##
-    # Authenticates with the server to get the authorization data. Raises an error if there is a problem
-    #
-    # @param [#to_s] account_id the account id
-    # @param [#to_s] application_key the private app key
-    # @param [Fixnum] default_concurrency default concurrency for a bucket.
-    #     Semi sain limits are imposed on this value so that you don't accidentally
-    #     use up all of your resources. Default limit is applied per bucket and
-    #     maxes out at 128, though you can increase per bucket.
-    # @raise [Backblaze::AuthError] when unable to authenticate
-    # @return [void]
-    def login(account_id:, application_key:, api_path: '/b2api/v1/', default_concurrency: 4)
-      options = {
-        basic_auth: {username: account_id, password: application_key}
-      }
-      api_path = "/#{api_path}/".gsub(/\/+/, '/')
-      response = HTTParty.get("https://api.backblazeb2.com#{api_path}b2_authorize_account", options)
-      raise Backblaze::AuthError.new(response) unless response.code == 200
+      ##
+      # Authenticates with the server to get the authorization data. Raises an error if there is a problem
+      #
+      # @param [#to_s] account_id the account id
+      # @param [#to_s] application_key the private app key
+      # @param [Fixnum] default_concurrency default concurrency for a bucket.
+      #     Semi sain limits are imposed on this value so that you don't accidentally
+      #     use up all of your resources. Default limit is applied per bucket and
+      #     maxes out at 128, though you can increase per bucket.
+      # @raise [Backblaze::AuthError] when unable to authenticate
+      # @return [void]
+      def login(account_id:, application_key:, api_path: '/b2api/v1/', default_concurrency: 4)
+        options = {
+          basic_auth: {username: account_id, password: application_key}
+        }
+        api_path = "/#{api_path}/".gsub(/\/+/, '/')
+        response = HTTParty.get("https://api.backblazeb2.com#{api_path}b2_authorize_account", options)
+        raise Backblaze::AuthError.new(response) unless response.code == 200
 
-      @account_id = response['accountId']
-      @token = response['authorizationToken']
-      @api_url = response['apiUrl']
-      @download_url = response['downloadUrl']
-      @api_path = api_path
-      @min_part_size = response['minimumPartSize']
-      @default_concurrency = Backblaze::Utils.limit(default_concurrency, 1..128) # limit to semi-reasonable defaults
+        @account_id = response['accountId']
+        @token = response['authorizationToken']
+        @api_url = response['apiUrl']
+        @download_url = response['downloadUrl']
+        @api_path = api_path
+        @min_part_size = response['minimumPartSize']
+        @default_concurrency = Backblaze::Utils.limit(default_concurrency, 1..128) # limit to semi-reasonable defaults
 
-      Backblaze::B2::Base.base_uri "#{@api_url}#{api_path}"
-      Backblaze::B2::Base.headers 'Authorization' => @token, 'Content-Type' => 'application/json'
-    end
+        Backblaze::B2::Base.base_uri "#{@api_url}#{api_path}"
+        Backblaze::B2::Base.headers 'Authorization' => @token, 'Content-Type' => 'application/json'
+      end
 
-    def credentials_file(filename, raise_errors: true, logging: false)
-      opts = nil
-      open(filename, 'r') do |f|
-        if ::File.extname(filename) == '.json'
-          require 'json'
-          opts = JSON.load(f)
+      def credentials_file(filename, raise_errors: true, logging: false)
+        opts = nil
+        open(filename, 'r') do |f|
+          if ::File.extname(filename) == '.json'
+            require 'json'
+            opts = JSON.load(f)
+          else
+            require 'psych'
+            opts = Psych.load(f.read)
+          end
+        end
+        parsed = {}
+        [:application_key, :account_id, :api_path].each do |key|
+          if opts[key.to_s].is_a? String
+            parsed[key] = opts[key.to_s]
+          end
+        end
+        if [:application_key, :account_id].inject(true) { |status, key| status && !parsed[key].nil? }
+          puts "Attempting #{parsed[:account_id]}" if logging
+          login(parsed)
+          puts "Login successful" if logging
+          true
         else
-          require 'psych'
-          opts = Psych.load(f.read)
+          puts "Missing params" if logging
+          false
         end
-      end
-      parsed = {}
-      [:application_key, :account_id, :api_path].each do |key|
-        if opts[key.to_s].is_a? String
-          parsed[key] = opts[key.to_s]
-        end
-      end
-      if [:application_key, :account_id].inject(true) { |status, key| status && !parsed[key].nil? }
-        puts "Attempting #{parsed[:account_id]}" if logging
-        login(parsed)
-        puts "Login successful" if logging
-        true
-      else
-        puts "Missing params" if logging
+      rescue => e
+        puts e if logging
+        raise e if raise_errors
         false
       end
-    rescue => e
-      puts e if logging
-      raise e if raise_errors
-      false
     end
   end
 end
--- lib/backblaze/b2/base.rb
+++ lib/backblaze/b2/base.rb
@@ -1,101 +1,103 @@
-module Backblaze::B2
-  class Base
-    include HTTParty
-    include Backblaze::Utils
+module Backblaze
+  module B2
+    class Base
+      include HTTParty
+      include Backblaze::Utils
 
-    format :json
+      format :json
 
-    # @!method get(path, options={}, &block)
-    # Calls the class level equivalent from HTTParty
-    # @see http://www.rubydoc.info/github/jnunemaker/httparty/HTTParty/ClassMethods HTTParty::ClassMethods
+      # @!method get(path, options={}, &block)
+      # Calls the class level equivalent from HTTParty
+      # @see http://www.rubydoc.info/github/jnunemaker/httparty/HTTParty/ClassMethods HTTParty::ClassMethods
 
-    # @!method head(path, options={}, &block)
-    # @see #get
+      # @!method head(path, options={}, &block)
+      # @see #get
 
-    # @!method post(path, options={}, &block)
-    # @see #get
+      # @!method post(path, options={}, &block)
+      # @see #get
 
-    # @!method put(path, options={}, &block)
-    # @see #get
+      # @!method put(path, options={}, &block)
+      # @see #get
 
-    [:get, :head, :post, :put].each do |req|
-      define_method(req) do |path, options={}, &block|
-        self.class.send(req, path, options, &block)
-      end
-    end
-
-    protected
-
-    def file_versions(bucket_id:, convert:, limit:, double_check_server:, file_name: nil, &block)
-      retreive_count = (double_check_server ? 0 : -1)
-      files = file_list(bucket_id: bucket_id, limit: limit, retreived: retreive_count, file_name: file_name, first_file: nil, start_field: 'startFileId'.freeze)
-
-      files.map! do |f|
-        if block.nil?
-          Backblaze::B2::FileVersion.new(f)
-        else
-          block.call(f)
+      [:get, :head, :post, :put].each do |req|
+        define_method(req) do |path, options={}, &block|
+          self.class.send(req, path, options, &block)
         end
-      end if convert
-      files.compact
-    end
-
-    def file_list(limit:, retreived:, first_file:, start_field:, bucket_id:, file_name: nil, first: true)
-      params = {'bucketId'.freeze => bucket_id}
-      if limit == -1
-        params['maxFileCount'.freeze] = 1000
-      elsif limit > 1000
-        params['maxFileCount'.freeze] = 1000
-      elsif limit > 0
-        params['maxFileCount'.freeze] = limit
-      else
-        return []
-      end
-      if first_file.nil?
-        if file_name && start_field == 'startFileId' && first
-          params['startFileName'] = file_name
-        end
-      else
-        params[start_field] = first_file
       end
 
-      response = post("/b2_list_file_#{start_field == 'startFileName' ? 'names' : 'versions'}", body: params.to_json)
+      protected
 
-      raise Backblaze::FileError.new(response) unless response.code == 200
+      def file_versions(bucket_id:, convert:, limit:, double_check_server:, file_name: nil, &block)
+        retreive_count = (double_check_server ? 0 : -1)
+        files = file_list(bucket_id: bucket_id, limit: limit, retreived: retreive_count, file_name: file_name, first_file: nil, start_field: 'startFileId'.freeze)
 
-      files = response['files'.freeze]
-      halt = false
-      files.map! do |f|
-        if halt
-          nil
-        else
-          ret = Hash[f.map{|k,v| [Backblaze::Utils.underscore(k).to_sym, v]}]
-          if file_name && file_name != ret[:file_name]
-            halt = true
+        files.map! do |f|
+          if block.nil?
+            Backblaze::B2::FileVersion.new(f)
+          else
+            block.call(f)
           end
-          halt ? nil : ret
-        end
-      end.compact!
-
-      retreived = retreived + files.size if retreived >= 0
-      if limit > 0
-        limit = limit - (retreived >= 0 ? files.size : 1000)
-        limit = 0 if limit < 0
+        end if convert
+        files.compact
       end
 
-      next_item = response[start_field.sub('start'.freeze, 'next'.freeze)]
+      def file_list(limit:, retreived:, first_file:, start_field:, bucket_id:, file_name: nil, first: true)
+        params = {'bucketId'.freeze => bucket_id}
+        if limit == -1
+          params['maxFileCount'.freeze] = 1000
+        elsif limit > 1000
+          params['maxFileCount'.freeze] = 1000
+        elsif limit > 0
+          params['maxFileCount'.freeze] = limit
+        else
+          return []
+        end
+        if first_file.nil?
+          if file_name && start_field == 'startFileId' && first
+            params['startFileName'] = file_name
+          end
+        else
+          params[start_field] = first_file
+        end
 
-      if (limit > 0 || limit == -1) && !!next_item && !halt
-        files.concat file_list(
-          first_file: next_item,
-          limit: limit,
-          retreived: retreived,
-          start_field: start_field,
-          bucket_id: bucket_id,
-          first: false
-        )
-      else
-        files
+        response = post("/b2_list_file_#{start_field == 'startFileName' ? 'names' : 'versions'}", body: params.to_json)
+
+        raise Backblaze::FileError.new(response) unless response.code == 200
+
+        files = response['files'.freeze]
+        halt = false
+        files.map! do |f|
+          if halt
+            nil
+          else
+            ret = Hash[f.map{|k,v| [Backblaze::Utils.underscore(k).to_sym, v]}]
+            if file_name && file_name != ret[:file_name]
+              halt = true
+            end
+            halt ? nil : ret
+          end
+        end.compact!
+
+        retreived = retreived + files.size if retreived >= 0
+        if limit > 0
+          limit = limit - (retreived >= 0 ? files.size : 1000)
+          limit = 0 if limit < 0
+        end
+
+        next_item = response[start_field.sub('start'.freeze, 'next'.freeze)]
+
+        if (limit > 0 || limit == -1) && !!next_item && !halt
+          files.concat file_list(
+            first_file: next_item,
+            limit: limit,
+            retreived: retreived,
+            start_field: start_field,
+            bucket_id: bucket_id,
+            first: false
+          )
+        else
+          files
+        end
       end
     end
   end
